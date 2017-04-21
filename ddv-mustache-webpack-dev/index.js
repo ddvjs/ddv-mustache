5763f1f914d465b49cff693b2dc7065a38fee89a
hua <yuchonghua@163.com>
2017-04-21 17:08:58 +0800

    fixbug version

--- ddv-mustache-webpack-dev/index.js
+++ ddv-mustache-webpack-dev/index.js
@@ -1,6 +1,9 @@
 'use strict'
 const spawn = require('cross-spawn')
 const logger = require('../build/logger.js')
+const packageJson = require('../package.json')
+const version = (packageJson && packageJson.devDependencies && packageJson.devDependencies['ddv-mustache-webpack-dev']) || ''
+const ddvMustacheWebpackDevVersion = version ? ('ddv-mustache-webpack-dev@' + version) : 'ddv-mustache-webpack-dev'
 module.exports = ddvMustacheWebpackDev
 function ddvMustacheWebpackDev (isAuto = true) {
   return new Promise((resolve, reject) => {
@@ -26,7 +29,7 @@ function ddvMustacheWebpackDevAutoInstall () {
 function ddvMustacheWebpackDevInstall () {
   logger.log('install module ddv-mustache-webpack-dev')
   return new Promise((resolve, reject) => {
-    var ls = spawn('npm', ['--save-dev', 'install', 'ddv-mustache-webpack-dev'], {
+    var ls = spawn('npm', ['--save-dev', 'install', ddvMustacheWebpackDevVersion], {
       cwd: process.cwd(),
       stdio: ['pipe', 'inherit', 'inherit']
     })
@@ -34,7 +37,7 @@ function ddvMustacheWebpackDevInstall () {
       if (code === 0) {
         resolve()
       } else {
-        reject(new Error('Please allow the command to install dependent packages: npm -D i ddv-mustache-webpack-dev'))
+        reject(new Error('Please allow the command to install dependent packages: npm -D i ' + ddvMustacheWebpackDevVersion))
       }
     })
   })
@@ -42,7 +45,7 @@ function ddvMustacheWebpackDevInstall () {
 function checkHasDdvMustacheWebpackDev () {
   logger.log('check has install module ddv-mustache-webpack-dev')
   return new Promise((resolve, reject) => {
-    var ls = spawn('npm', ['ls', 'ddv-mustache-webpack-dev'], {
+    var ls = spawn('npm', ['ls', ddvMustacheWebpackDevVersion], {
       cwd: process.cwd(),
       stdio: ['pipe', 'inherit', 'inherit']
     })
